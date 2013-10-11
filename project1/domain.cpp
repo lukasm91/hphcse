c6309f1daeb0ba207ce23e619662b47b251a6c9f
zemanc <zemanc@student.ethz.ch>
2013-10-11 19:58:55 +0200

    some new stuff

--- project1/domain.cpp
+++ project1/domain.cpp
@@ -1,38 +1,84 @@
 // domain.cpp
 #include "domain.h"
 
-// Constructor for domain with particles
-Domain::Domain(int n){
-	// add particles
-	for (int i=1; i<n; ++i)
-		add_particle();
-}
-
-void Domain::add_particle(){
-	// initialize particle
-	Particle p;
-	for (int i=0; i<2; ++i) {
-		p.p1[i] = uniform(generator);	// position uniformely distributed
-		p.v1[i] = normal(generator);	// velocity normal distributed
+// Constructor
+Domain::Domain(int n) : n_pt(n) {
+	// create arrays for particles and random numbers
+	*particles = new Particle[n];
+	nn = 3*n;	// we need 3 random numbers per particle and initial condition
+	int m = (nn%2 == 0) ? nn/2 : (nn+1)/2;
+	double *uniform = new double[nn];
+	double *normal = new double[2*m];
+	// add uniformely distributed numbers to array
+	for (int i=0; i<nn; ++i)
+		uniform[i] = ((drand48()-0.5)+(ub+lb)/2)*abs(ub-lb);
+	// add normal distributed (Box-Muller) numbers to array
+	double r1;
+	double r2;
+	for (int i=0; i<m; ++i) {
+		r1 = ((drand48()-0.5)+(ub+lb)/2)*abs(ub-lb);
+		r2 = ((drand48()-0.5)+(ub+lb)/2)*abs(ub-lb);
+		normal[2*i] = sqrt(-2*log(r1))*cos(2*pi*r2);
+		normal[2*i+1] = sqrt(-2*log(r1))*sin(2*pi*r2);
 	}
-	// add particle to vector
-	particle.push_back(p);
-}
-
-void calc_acc(){
-	for (unsigned i=0; i < particles.size(); ++i) {
-		for (unsigned j=0; j < particles.size(); ++j) {
-			if (i != j) {
-				// calculate force
-			}
+	// add particles
+	for (int i=0; i<n; ++i) {
+		for (int j=0; j<3; ++j) {
+			particles[i].p1[j] = uniform[3*i+j];
+			particles[i].v1[j] = normal[3*i+j];
 		}
 	}
+	// calc acceleration
+	calc_acc();
+	// clean up
+	delete[] uniform;
+	delete[] normal;
 }
-	
+
+// Destructor 
+Domain::~Domain() {
+	delete[] particles;
+}
+
+double get_distance(Particle PA, Particle PB) {
+	double r = abs(sqrt( (PA.p1[0]-PB.p1[0])^2
+						+(PA.p1[1]-PB.p1[1])^2
+						+(PA.p1[2]-PB.p1[2])^2 ));
+	return r;
 
 }
 
-void Domain::next_timestep(double t){
+void Domain::calc_acc() {
+	double r_vec[3];	// distance vector between 2 particles
+	double f_vec[3];	// force vector between 2 particles
+	double r;			// abs distance between 2 particles
+	for (unsigned i=0; i < n_pt; ++i) {
+		for (unsigned j=0; j < n_pt; ++j) {
+			if (i != j) {
+				// calculate force and acceleration on particle
+				r_vec[0] = particles[i].p1[0] - particles[j].p1[0];
+				r_vec[1] = particles[i].p1[1] - particles[j].p1[1];
+				r_vec[2] = particles[i].p1[2] - particles[j].p1[2];
+				r = get_distance(particles[i], particles[j]);
+				for (unsigned k=0; k < 3; ++k) {
+					f_vec[k] =  f_vec[k]
+							-24*eps*(2*(s_lj/r)^12 - (s_lj/r)^6)*r_vec[k]/r^2;
+				}
+			}
+		}
+		for (unsigned k=0; k<3; ++k)
+			particles[i].a2[k] += f_vec[k];
+	}
+}
 
+void Domain::calc_pos(double t) {
+// TODO
+}
 
+void Domain::calc_vel(double t) {
+// TODO
+}
+
+void Domain::next_timestep(double t) {
+// TODO
 }
--- project1/domain.h
+++ project1/domain.h
@@ -2,43 +2,42 @@
 #ifndef _DOMAIN_H
 #define _DOMAIN_H
 
-#include <vector>
-#include <random>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
 
 // some constants
 #define lb = -1;
 #define ub = 1;
-#define meanv = 0;
-#define sigma = 0.5;
+#define eps = 1;
+#define pi = 3.14159265359;
+
+// reinitialization for drand48()
+srand48( static_cast<unsigned>(std::time(NULL)) );
+
 
 struct Particle{
 	const double mass = 1;
 	// position vectors
-	std::vector<double>(3) p1;  // x_i
-	std::vector<double>(3) p2;  // x_i+1
-	// velocity vectors
-	std::vector<double>(3) v1;  // v_i
-	std::vector<double>(3) v2;  // v_i+1
-	// acceleration vectors
-	std::vector<double>(3) a1;  // a_i
-	std::vector<double>(3) a2;  // a_i+1
+	double p1[3], p2[3];	// position arrays
+	double v1[3], v2[3];	// velocity arrays
+	double a1[3], a2[3];	// acceleration arrays
 };
 
 class Domain{
 private:
-	// generate random numbers
-	std::default_random_engine generator;
-	std::uniform_real_distribution<double> uniform(lb, ub);
-	std::normal_distribution<double> normal(meanv, sigma);
-
-	std::vector<Particle> particles; 	// particles
-	void calc_acc();					// calculate accelerations
-	void calc_pos(double);		// calculate new positions
-	void calc_vel(double);		// calculate new velocities
+	Particles *particles; 			// particles
+	int n_pt;						// number of particles
+	double r_vec[3];				// distance vector between 2 particles
+	double f_vec[3];				// force vector between 2 particles
+	void calc_acc();				// calculate acceleration
+	void calc_pos(double t);		// calculate new positions
+	void calc_vel(double t);		// calculate new velocities
 public:
 	Domain(int);					// Constructor
-	void add_particle();			// add particle to domain
-	void next_timestep(double);		// go one timestep further
+	~Domain();						// Destructor
+	void next_timestep(double t);	// go one timestep further
+	double get_distance(Particle, Particle);			// get abs distance
 };
 
 #endif
