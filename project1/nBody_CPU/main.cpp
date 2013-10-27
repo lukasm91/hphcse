8ebf018d4e1d096732e7fcdd4a76be61c1837f02
zemanc <zemanc@student.ethz.ch>
2013-10-27 09:46:11 +0100

    arguments

--- project1/nBody_CPU/main.cpp
+++ project1/nBody_CPU/main.cpp
@@ -6,68 +6,111 @@
 #include <fstream>
 #include "domain.h"
 #include "ArgumentParser.h"
+#include "Timer.h"
 
-void prtout(std::ofstream &out, Domain &d, unsigned i, unsigned n) {
-	out << "Timestep " << i << std::endl;
-	out << "-----------------------------------------------------" << std::endl;
-	for (unsigned j=0; j<n; ++j) {
-		out << "Particle " << j << ":" << std::endl;
-		out << "\t" << "x:" << "\t" << d.prt_x[3*(2*j+1)] << "\t" << d.prt_x[3*(2*j+1)+1]
-				<< "\t" << d.prt_x[3*(2*j+1)+2] << std::endl;
-		out << "\t" << "v:" << "\t" << d.prt_v[3*(2*j+1)] << "\t" << d.prt_v[3*(2*j+1)+1]
-				<< "\t" << d.prt_v[3*(2*j+1)+2] << std::endl;
-		out << "\t" << "a:" << "\t" << d.prt_a[3*(2*j+1)] << "\t" << d.prt_a[3*(2*j+1)+1]
+// ================================================================
+// output function
+// ================================================================
+void prtout(std::ofstream &out, Domain &d, unsigned i, unsigned n, bool hdr) {
+	if (hdr) {
+		out << "it" << "\t" << "prt" << "\t" << "x_1" << "\t" << "x_2" << "\t" << "x_3"
+			<< "\t" << "v_1" << "\t" << "v2" << "\t" << "v3" << "\t"
+			<< "a_1" << "\t" << "a_2" << "\t" << "a_3" << std::endl;
+	} else {
+		for (unsigned j=0; j<n; ++j) {
+			out << i << "\t" << j << "\t" << d.prt_x[3*(2*j+1)] << "\t"
+				<< d.prt_x[3*(2*j+1)+1]
+				<< "\t" << d.prt_x[3*(2*j+1)+2]
+				<< "\t" << d.prt_v[3*(2*j+1)] << "\t" << d.prt_v[3*(2*j+1)+1]
+				<< "\t" << d.prt_v[3*(2*j+1)+2]
+				<< "\t" << d.prt_a[3*(2*j+1)] << "\t" << d.prt_a[3*(2*j+1)+1]
 				<< "\t" << d.prt_a[3*(2*j+1)+2] << std::endl;
+		}
 	}
-	out << std::endl;
 }
 
-
-int main(int argc, char *argv[])
+// ================================================================
+// nBody simulation
+// ================================================================
+void nBody(unsigned t, unsigned n, bool print)
 {	
-	ArgumentParser parser(argc, argv);
-	const unsigned t = parser("-t").asInt(10);		// number of steps, default 10
-	const unsigned n = parser("-n").asInt(10);		// number of particles, default 10
-
 	// constants
 	const double m = 1;				// mass of a particle
 	const double epsilon = 1;		// epsilon (Lennard-Jones)
-	const double sigma = 0.1;		// sigma (Lennard-Jones)
+	const double sigma = 0.2;		// sigma (Lennard-Jones)
 	const double lb = -1;			// lower boundary
 	const double ub = 1;			// upper boundary
 	const double dt = 0.0001;		// timestep size
 
-	// files for output
-	std::ofstream outfile("nbody.out", std::ios::out);
-// 	std::ofstream prtrack("prttrack.out", std::ios::out);
-	outfile.precision(4);
-// 	prtrack.precision(4);
-
 	// create domain
 	Domain d = Domain(n, m, dt, epsilon, sigma, lb, ub);
-	outfile << std::fixed
-			<< "i" << "\t" << "E_Kin" << "\t" << "E_Pot" << "\t" << "E_Tot"
-			<< "\t" << "C_x" << "\t\t" << "C_y" << "\t\t" << "C_z"
-			<< "\t\t" << "P_x" << "\t\t" << "P_y" << "\t\t" << "P_z"
-			<< "\t\t" << "L_x" << "\t\t" << "L_y" << "\t\t" << "L_z" << std::endl;
-	outfile << std::endl;
+	
+	// files for output
+	std::ofstream outfile("nbody.out", std::ios::out);
+	std::ofstream prtrack("prttrack.out", std::ios::out);
+	outfile.precision(4);
+	prtrack.precision(4);
+	
+	if (print) {
+		prtout(prtrack, d, 0, n, 1);
+		outfile << std::fixed
+				<< "i" << "\t" << "E_Kin" << "\t" << "E_Pot" << "\t" << "E_Tot"
+				<< "\t" << "C_x" << "\t\t" << "C_y" << "\t\t" << "C_z"
+				<< "\t\t" << "P_x" << "\t\t" << "P_y" << "\t\t" << "P_z"
+				<< "\t\t" << "L_x" << "\t\t" << "L_y" << "\t\t" << "L_z" << std::endl;
+		outfile << std::endl;
+	}
 
 	// timesteps
 	for (unsigned i=0; i<t; ++i) {
-// 		prtout(prtrack, d, i, n);
+		if (print) {
+			prtout(prtrack, d, i, n, 0);
+			outfile << std::fixed
+					<< i << "\t" << d.eKin << "\t" << d.ePot << "\t" << d.eTot
+					<< "\t" << d.ctr_m[0] << "\t" << d.ctr_m[1] << "\t" << d.ctr_m[2]
+					<< "\t" << d.tot_lm[0] << "\t" << d.tot_lm[1] << "\t" << d.tot_lm[2] 
+					<< "\t" << d.tot_am[0] << "\t" << d.tot_am[1] << "\t" << d.tot_am[2] << std::endl;
+		}
+		d.next_timestep();
+	}
+	if (print) {
+	prtout(prtrack, d, t, n, 0);
 		outfile << std::fixed
-				<< i << "\t" << d.eKin << "\t" << d.ePot << "\t" << d.eTot
+				<< t << "\t" << d.eKin << "\t" << d.ePot << "\t" << d.eTot
 				<< "\t" << d.ctr_m[0] << "\t" << d.ctr_m[1] << "\t" << d.ctr_m[2]
 				<< "\t" << d.tot_lm[0] << "\t" << d.tot_lm[1] << "\t" << d.tot_lm[2] 
 				<< "\t" << d.tot_am[0] << "\t" << d.tot_am[1] << "\t" << d.tot_am[2] << std::endl;
-		d.next_timestep();
 	}
-// 	prtout(prtrack, d, t, n);
-	outfile << std::fixed
-			<< t << "\t" << d.eKin << "\t" << d.ePot << "\t" << d.eTot
-			<< "\t" << d.ctr_m[0] << "\t" << d.ctr_m[1] << "\t" << d.ctr_m[2]
-			<< "\t" << d.tot_lm[0] << "\t" << d.tot_lm[1] << "\t" << d.tot_lm[2] 
-			<< "\t" << d.tot_am[0] << "\t" << d.tot_am[1] << "\t" << d.tot_am[2] << std::endl;
+}
+
+// ================================================================
+// main with timer
+// ================================================================
+
+int main (int argc, char *argv[])
+{
+	// arguments
+	ArgumentParser parser(argc, argv);
+	const unsigned it = parser("-i").asInt(1);			// number of iterations, default 1
+	const unsigned t = parser("-t").asInt(100);			// number of timesteps
+	unsigned n = parser("-n").asInt(2);					// initial number of particles, default 2
+	const bool print = parser("-p").asBool(1);			// print outfile?
+	
+	// timing
+	std::ofstream times("timing.out", std::ios::out);
+	times << "i" << "\t" << "n" << "\t" << "time" << std::endl;
+	times << "-------------------------------------------" << std::endl;
+	Timer timer; // declaration of the timer
+
+	for (unsigned i=0; i<it; ++i) {
+		times << i << "\t" << n << "\t";
+		timer.start();
+		nBody(t, n, print);
+		times << timer.stop() << std::endl;
+		n *= 2;
+	}
+		
 	return 0;
 }
+
 
